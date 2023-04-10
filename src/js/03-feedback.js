6adf7dd7e2410aa3dae9cd2dfef2fe7e0f27a05d
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-04-10 10:21:51 +0300

    exercise 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const keyForm = "feedback-form-state";
+let formValues = JSON.parse(localStorage.getItem(keyForm));
+
+const { email, message } = form.elements;
+
+const saveLocal = throttle(event => {
+    formValues = { email: email.value, message: message.value };
+    localStorage.setItem(keyForm, JSON.stringify(formValues));
+}, 500);
+
+if (formValues) {
+    email.value = formValues.email;
+    message.value = formValues.message;
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    localStorage.clear;
+
+    if (email.value === '' || message.value === '') {
+        return console.log('All fields must be filled');
+    }
+    console.log(formValues);
+    event.currentTarget.reset();
+}
+
+form.addEventListener('input', saveLocal);
+form.addEventListener('submit', onSubmit);
